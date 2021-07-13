fdd013432461deb3b5daa8bc2fa60e8e792303cc
rohangrge <rohangrge@gmail.com>
2021-07-13 10:51:40 +0530

    adding error handling

--- src/main/java/com/reviewservice/learning/error.java
+++ src/main/java/com/reviewservice/learning/error.java
@@ -1,5 +1,16 @@
 package com.reviewservice.learning;
 
 public class error {
-    private static String err=""
+    private String err="requested content is not found";
+    private int id1;
+    public error(int id,String content){
+        this.err=content;
+        this.id1=id;
+    }
+    public int getId(){
+        return id1;
+    }
+    public String getErr(){
+        return err;
+    }
 }
